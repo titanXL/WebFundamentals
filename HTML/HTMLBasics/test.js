887fbe7ea769e65fc893570715039cc5d175e7c3
titanXL <emildavidkov88@gmail.com>
2016-08-12 21:01:55 +0300

    tables

--- HTML/HTMLBasics/test.js
+++ HTML/HTMLBasics/test.js
@@ -1,4 +1,5 @@
 function onClick(){
+    
     var effectedElement = document.getElementById('home');
     effectedElement.style.backgroundColor = 'orangeRed';
     effectedElement.style.backgroundR
